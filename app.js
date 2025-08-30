a72fc56f18c0caa21e2a53781f93f5205f6749bd
Theo Back <theobackbernardes@gmail.com>
2025-08-29 21:57:28 -0300

    endpoints no app.js

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ app.listen(PORT, () => {
     console.log('Para parar o servidor, pressione Ctrl + C no terminal');
 });
 
-
 app.get('/', (req, res) => {
     res.send('Olá, mundo! Este é meu primeiro backend com Express.');
 });
+
+app.get('/sobre', (req, res) => {
+    res.send('Esta é a página sobre mim. Estou aprendendo backend com Express!');
+});
+
+const produtos = [
+    { id: 1, nome: 'Tecaldo mecânico', preco: 450.00 },
+    { id: 2, nome: 'Mouse Gamer', preco: 150.00 },
+    { id: 3, nome: 'Monitor UltraWide', preco: 1200.00 },
+];
+
+app.get('/api/produtos', (req, res) => {   
+    // res.json() envia um objeto ou array
+    res.json(produtos);
+}); 
